77d071ec127c053e0eda1b57b11ebdacdbfa3f0c
Simon Pannek <simon.pannek@tum.de>
2021-05-09 12:27:00 +0200

    Add code examples for async client and server

---
+++ examples/async_client/src/main.rs
@@ -0,0 +1,41 @@
+use tokio::{
+    io::{stdin, AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+};
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect(ADDRESS).await?;
+
+    let mut reader = BufReader::new(stdin());
+
+    loop {
+        let mut buf = String::new();
+
+        match reader.read_line(&mut buf).await {
+            Ok(_) => {
+                // Close connection if client writes "exit"
+                if buf.trim().to_lowercase() == "exit" {
+                    stream.shutdown().await?;
+                    break;
+                }
+
+                // Send buffer to server
+                stream
+                    .write_all(buf.as_bytes())
+                    .await
+                    .expect("Failed to write to server");
+            }
+            Err(e) => {
+                // Print read error and shut down stream
+                eprintln!("An error occured when reading from stdin: {}", e);
+                stream.shutdown().await?;
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ examples/async_server/src/main.rs
@@ -0,0 +1,54 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
+async fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    tokio::spawn(async move {
+        println!("New connection.");
+
+        let mut reader = BufReader::new(&mut stream);
+
+        loop {
+            let mut buf = String::new();
+
+            match reader.read_line(&mut buf).await {
+                Ok(size) => {
+                    // Empty buffer means EOF (closed connection)
+                    if size == 0 {
+                        break;
+                    }
+
+                    // Print received data
+                    print!("{}", buf);
+                }
+                Err(e) => {
+                    // Print read error and shut down stream
+                    eprintln!("An error occured when reading from socket: {}", e);
+                    stream
+                        .shutdown()
+                        .await
+                        .expect("Failed to shutdown connection");
+                    break;
+                }
+            }
+        }
+
+        println!("Connection closed.");
+    });
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind(ADDRESS).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        handle_client(stream).await?;
+    }
+}
