27ae620d8febe67742b28b983c7fe1d6756b458c
Simon Pannek <simon.pannek@tum.de>
2021-05-10 19:19:06 +0200

    Add second subsection of chapter 3 (former chapter 4)

--- examples/futures/src/main.rs
+++ examples/futures/src/main.rs
@@ -1,18 +1,20 @@
 use std::time::Duration;
 use tokio::{join, time::sleep};
 
+async fn async_function() {
+    println!("Started task1");
+    sleep(Duration::from_secs(5)).await;
+    println!("Finished task1");
+}
+
 #[tokio::main]
 async fn main() {
-    let future1 = async move {
-        println!("Started future1");
-        sleep(Duration::from_secs(5)).await;
-        println!("Finished future1");
-    };
+    let future1 = async_function();
 
-    let future2 = async move {
-        println!("Started future2");
+    let future2 = async {
+        println!("Started task2");
         sleep(Duration::from_secs(3)).await;
-        println!("Finished future2");
+        println!("Finished task2");
     };
 
     // Join both futures
